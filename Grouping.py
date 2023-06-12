ec60b6ee2e0b6db5fc73b3298df0edafe7eb775a
Behnam Baloochy <baloochy@gmail.com>
2023-06-12 09:38:56 +0330

    Grouping

--- Grouping.py
+++ Grouping.py
@@ -1,2 +1,15 @@
 import numpy as np
 import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
+        "B": ["one", "one", "two", "three", "two", "two", "one", "three"],
+        "C": np.random.randn(8),
+        "D": np.random.randn(8),
+    }
+)
+print(df)
+print(df.groupby("A")[["C", "D"]].sum())
+df1 = df.groupby(["A", "B"]).sum()
+print(df1)
